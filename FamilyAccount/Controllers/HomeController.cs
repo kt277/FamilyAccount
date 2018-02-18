31581c2957b5c6293b6be4f25ce35a016053b903
酱 番茄 <kt277@msn.com>
2018-02-18 11:35:44 +0800

    模板化修正

--- FamilyAccount/Controllers/HomeController.cs
+++ FamilyAccount/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace FamilyAccount.Controllers
             return View();
         }
 
+        public ActionResult LightBox()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
